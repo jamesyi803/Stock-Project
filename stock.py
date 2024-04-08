2eb53fad46d545f0b5a32fd8aa8e21cd6d20bf74
James Yi <jamesyi803@gmail.com>
2024-04-08 15:15:15 -0700

    updated

--- stock.py
+++ stock.py
@@ -44,16 +44,16 @@ spy["Target"] = spy["Tomorrow"] > spy["Close"].astype(int)
 btc["Target"] = btc["Tomorrow"] > btc["Close"].astype(int)
 eth["Target"] = eth["Tomorrow"] > eth["Close"].astype(int)
 # machine learning model
-model = RandomForestClassifier(n_estimators=200, min_samples_split=50, random_state=1)
+model = RandomForestClassifier(n_estimators=300, min_samples_split=25, random_state=1)
 
 # train and test
-train_spy = spy.iloc[:-100]
-train_btc = btc.iloc[:-100]
-train_eth = eth.iloc[:-100]
+train_spy = spy.iloc[:-200]
+train_btc = btc.iloc[:-200]
+train_eth = eth.iloc[:-200]
 
-test_spy = spy.iloc[-100:]
-test_btc = btc.iloc[-100:]
-test_eth = eth.iloc[-100:]
+test_spy = spy.iloc[-200:]
+test_btc = btc.iloc[-200:]
+test_eth = eth.iloc[-200:]
 
 # predict
 predictors = ["Close", "Volume", "Open", "High", "Low"]
@@ -115,10 +115,6 @@ def backtest_eth(data,model,predictors,start=2000, step=200):
         all_predictions.append(predictions)
     return pd.concat(all_predictions)
 
-predictions_spy = backtest_spy(spy,model,predictors)
-predictions_btc = backtest_btc(btc,model,predictors)
-predictions_eth = backtest_eth(eth,model,predictors)
-
 
 # count how many of each prediction was made
 value_spy = predictions_spy["Predictions"].value_counts()
@@ -130,16 +126,52 @@ precision_score_spy = precision_score(predictions_spy["Target"],predictions_spy[
 precision_score_btc = precision_score(predictions_btc["Target"],predictions_btc["Predictions"])
 precision_score_eth = precision_score(predictions_eth["Target"],predictions_eth["Predictions"])
 
-print("The accuracy score for spy in the past 2000 trading days is: " + str(precision_score_spy))
-print("The accuracy score for btc in the past 2000 trading days is: " + str(precision_score_btc))
-print("The accuracy score for eth in the past 2000 trading days is: " + str(precision_score_eth))
+# print("The accuracy score for spy in the past 2000 trading days is: " + str(precision_score_spy))
+# print("The accuracy score for btc in the past 2000 trading days is: " + str(precision_score_btc))
+# print("The accuracy score for eth in the past 2000 trading days is: " + str(precision_score_eth))
 
 percent_spy = predictions_spy["Target"].value_counts() / predictions_spy.shape[0]
 percent_btc = predictions_btc["Target"].value_counts() / predictions_btc.shape[0]
 percent_eth = predictions_eth["Target"].value_counts() / predictions_eth.shape[0]
 
-print("The percentage for of days spy went up is " + str(percent_spy))
-print("The percentage for of days btc went up is " + str(percent_btc))
-print("The percentage for of days eth went up is " + str(percent_eth))
+# print("The percentage for of days spy went up is " + str(percent_spy))
+# print("The percentage for of days btc went up is " + str(percent_btc))
+# print("The percentage for of days eth went up is " + str(percent_eth))
 
-# print(precision_score)
+horizons = [2,5,60,250,1000]
+
+new_predictors_spy = []
+new_predictors_btc = []
+new_predictors_eth = []
+
+for horizon in horizons:
+    rolling_avg_spy = spy.rolling(horizon).mean()
+    rolling_avg_btc = btc.rolling(horizon).mean()
+    rolling_avg_eth = eth.rolling(horizon).mean()
+
+    ratio_column = f"Close_Ratio_{horizon}"
+    spy[ratio_column] = spy["Close"] / rolling_avg_spy["Close"]
+    btc[ratio_column] = btc["Close"] / rolling_avg_btc["Close"]
+    eth[ratio_column] = eth["Close"] / rolling_avg_eth["Close"]
+
+    trend_column = f"Trend_{horizon}"
+    spy[trend_column] = spy.shift(1).rolling(horizon).sum()["Target"]
+    btc[trend_column] = btc.shift(1).rolling(horizon).sum()["Target"]
+    eth[trend_column] = eth.shift(1).rolling(horizon).sum()["Target"]
+
+    new_predictors_spy += [ratio_column,trend_column]
+    new_predictors_btc += [ratio_column,trend_column]
+    new_predictors_eth += [ratio_column,trend_column]
+
+spy = spy.dropna()
+btc = btc.dropna()
+eth = eth.dropna()
+
+predictions_spy = backtest_spy(spy,model,new_predictors_spy)
+predictions_btc = backtest_btc(btc,model,new_predictors_btc)
+predictions_eth = backtest_eth(eth,model,new_predictors_eth)
+
+
+print(predictions_spy["Predictions"].value_counts())
+print(predictions_btc["Predictions"].value_counts())
+print(predictions_eth["Predictions"].value_counts())
